20fbd0339db2e1b7530c8ae8228aa013e1c2915e
Sagor Chanda <ch.sagor27@gmail.com>
2026-05-20 14:41:12 +0600

    palindrome 9

---
+++ Easy/Math/palindrome-9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        original = x
+        rev = 0
+        while(x):
+            rev = rev * 10 + x%10
+            x //=10
+        return rev == original
+sol = Solution()
+print(sol.isPalindrome(121))
